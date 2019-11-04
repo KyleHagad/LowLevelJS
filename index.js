9a40cb81ff345eaa3149b61d09faeb8308d389a9
khagadorn <xekyle@gmail.com>
2019-11-04 10:57:18 -0700

    String parser receives and returns an object-encoded state

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-const str = s => targetString => {
+const str = s => parserState => { // receives a state Object.
+	const {
+		targetString,
+		index,
+	} = parserState;
 
+	if (targetString.slice(index).startsWith(s)) { // returns a state Object.
+		return { // Success
+			...parserState,
+			result: s,
+			index: index + s.length,
+		};
+	}
+
+	throw new Error (`Failed to find ${s}; Instead found ${targetString.slice(index, index + 10)}`)
 };
+
+// shape of functional flow can be summarized as ` ParserState IN >-> ParserState OUT `
+const run = (parser, targetString) => {
+	const initState = {
+		targetString,
+		index: 0,
+		result: undefined,
+	};
+	return parser(initState);
+};
+
+// Parsing at work
+const parser = str('hello there');
+
+console.log(run(parser, 'hello there'));
